a7875875ce3359665739c4870f0069e28e2b0d7e
tobi <>
1999-12-05 10:44:13 +0000

    Undone changes by sms

--- src/ulcomp.c
+++ src/ulcomp.c
@@ -84,9 +84,8 @@ static void parse2d(char *ptr, unsigned int *net, unsigned int *node)
         *net = 10 * (*net) + (*ptr)-'0';
         ptr++;
     }
-    if ((*ptr) != '/')
+    if ((*(ptr++)) != '/')
     {
-        ptr++;
         *net = *node = 0;
     }
     else
